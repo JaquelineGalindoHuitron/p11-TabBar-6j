ccc05d4497211c220ef00ac73fd1cd89c17e94a7
JAQUELINE_GALINDO_HUITRON <a.21308051280478@cbtis128.edu.mx>
2024-02-29 17:13:32 +0000

    actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,83 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppTabBar());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class AppTabBar extends StatelessWidget {
+  const AppTabBar({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: "Ejemplo TabBar",
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: MiPaginaInicial(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MiPaginaInicial extends StatefulWidget {
+  const MiPaginaInicial({Key? key}) : super(key: key);
 
   @override
+  State<MiPaginaInicial> createState() => _MiPaginaInicialState();
+}
+
+class _MiPaginaInicialState extends State<MiPaginaInicial> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("TabBar Jaqueline Galindo"),
+            bottom: TabBar(tabs: [
+              Tab(
+                text: "Spa",
+                icon: Icon(Icons.add_alert),
+              ),
+              Tab(
+                text: "Contac",
+                icon: Icon(Icons.call_missed),
+              ),
+              Tab(
+                text: "Nosotro",
+                icon: Icon(Icons.people_alt_rounded),
+              ),
+              Tab(
+                text: "ubica",
+                icon: Icon(Icons.location_city_sharp),
+              ),
+            ] //texto icono
+                ),
+          ),
+          body: TabBarView(
+            children: const <Widget>[
+              Center(
+                child: Text(
+                  "Alerta",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+                ),
+              ),
+              Center(
+                child: Text("Opcion2",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 50)),
+              ),
+              Center(
+                child: Text(
+                  "Opcion 3",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+                ),
+              ),
+              Center(
+                child: Text(
+                  "Opcion 4",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
